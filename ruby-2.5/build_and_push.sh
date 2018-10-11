3b19546b6e4677d8cbc5d970ec20428c914bf6a4
Kasper Grubbe <kawsper@gmail.com>
2018-10-11 11:40:22 +0100

    Test if docker file is present

--- ruby-2.5/build_and_push.sh
+++ ruby-2.5/build_and_push.sh
@@ -12,6 +12,11 @@ then
   exit 1;
 fi
 
+if [ ! -f $DOCKERFILE ]; then
+  echo "Can't find $DOCKERFILE"
+  exit 1;
+fi
+
 echo "Building and pushing ${REPOOWNER}/${REPONAME} version ${VERSION}" \
 && docker build -t ${REPONAME}:${VERSION} -f ${DOCKERFILE} . \
 && docker tag ${REPONAME}:${VERSION} ${REPOOWNER}/${REPONAME}:${VERSION} \
