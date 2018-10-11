cfcb4656995087788e133b4d96a8ddb9fc26f774
Kasper Grubbe <kawsper@gmail.com>
2018-10-11 11:34:52 +0100

    Add a test to see if we are in root of the repo

--- ruby-2.4/build_and_push.sh
+++ ruby-2.4/build_and_push.sh
@@ -3,9 +3,16 @@ set -euxo pipefail
 
 REPOOWNER="kaspergrubbe"
 REPONAME="grubruby-jemalloc"
-VERSION="2.4.4.1"
+VERSION="2.4.4.2"
+DOCKERFILE="ruby-2.4/Dockerfile"
+
+if [[ "$PWD" != */grubruby ]]
+then
+  echo "You need to be in root dir of the repo!"
+  exit 1;
+fi
 
 echo "Building and pushing ${REPOOWNER}/${REPONAME} version ${VERSION}" \
-&& docker build -t ${REPONAME}:${VERSION} -f Dockerfile . \
+&& docker build -t ${REPONAME}:${VERSION} -f ${DOCKERFILE} . \
 && docker tag ${REPONAME}:${VERSION} ${REPOOWNER}/${REPONAME}:${VERSION} \
 && docker push ${REPOOWNER}/${REPONAME}:${VERSION}
--- ruby-2.5/build_and_push.sh
+++ ruby-2.5/build_and_push.sh
@@ -4,8 +4,15 @@ set -euxo pipefail
 REPOOWNER="kaspergrubbe"
 REPONAME="grubruby-jemalloc"
 VERSION="2.5.1"
+DOCKERFILE="ruby-2.5/Dockerfile"
+
+if [[ "$PWD" != */grubruby ]]
+then
+  echo "You need to be in root dir of the repo!"
+  exit 1;
+fi
 
 echo "Building and pushing ${REPOOWNER}/${REPONAME} version ${VERSION}" \
-&& docker build -t ${REPONAME}:${VERSION} -f Dockerfile . \
+&& docker build -t ${REPONAME}:${VERSION} -f ${DOCKERFILE} . \
 && docker tag ${REPONAME}:${VERSION} ${REPOOWNER}/${REPONAME}:${VERSION} \
 && docker push ${REPOOWNER}/${REPONAME}:${VERSION}
