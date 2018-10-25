1270ec80f786811c632bb9ae7e4b44ebeda46392
Kasper Grubbe <kawsper@gmail.com>
2018-10-25 11:28:34 +0100

    Use —tag to be explicit

--- ruby-2.5/build_and_push.sh
+++ ruby-2.5/build_and_push.sh
@@ -12,6 +12,6 @@ if [ ! -f $DOCKERFILE ]; then
 fi
 
 echo "Building and pushing ${REPOOWNER}/${REPONAME} version ${VERSION}" \
-&& docker build -t ${REPONAME}:${VERSION} -f ${DOCKERFILE} . \
+&& docker build --tag ${REPONAME}:${VERSION} -f ${DOCKERFILE} . \
 && docker tag ${REPONAME}:${VERSION} ${REPOOWNER}/${REPONAME}:${VERSION} \
 && docker push ${REPOOWNER}/${REPONAME}:${VERSION}
