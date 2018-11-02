3781530f6c0bbcc837ff59f811c197d17783956e
Kasper Grubbe <kawsper@gmail.com>
2018-11-02 12:30:31 +0000

    Upgrade rubygems to 2.7.8

--- tools/build.rb
+++ tools/build.rb
@@ -1,7 +1,7 @@
 grubruby_repoowner = 'kaspergrubbe'
 grubruby_reponame  = 'grubruby-jemalloc'
 grubruby_version   = '8003'
-rubygems_version   = '2.7.7'
+rubygems_version   = '2.7.8'
 bundler_version    = '1.17.1'
 
 def run_command(command)
