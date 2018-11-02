49c548084d774c7448b05065171300cb19c14220
Kasper Grubbe <kawsper@gmail.com>
2018-11-02 12:44:54 +0000

    Make a release for version 8004

--- tools/build.rb
+++ tools/build.rb
@@ -1,6 +1,6 @@
 grubruby_repoowner = 'kaspergrubbe'
 grubruby_reponame  = 'grubruby-jemalloc'
-grubruby_version   = '8003'
+grubruby_version   = '8004'
 rubygems_version   = '2.7.8'
 bundler_version    = '1.17.1'
 
