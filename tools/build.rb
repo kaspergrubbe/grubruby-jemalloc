6afc815a7002939a7e3e3e61e9d89c714740c0a6
Kasper Grubbe <kawsper@gmail.com>
2018-12-26 20:29:18 +0100

    Update rubygems to 3.0.1

--- tools/build.rb
+++ tools/build.rb
@@ -1,7 +1,7 @@
 grubruby_repoowner = 'kaspergrubbe'
 grubruby_reponame  = 'grubruby-jemalloc'
 grubruby_version   = '8006'
-rubygems_version   = '3.0.0'
+rubygems_version   = '3.0.1'
 bundler_version    = '1.17.2'
 
 def run_command(command)
