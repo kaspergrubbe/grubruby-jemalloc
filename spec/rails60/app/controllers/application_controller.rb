1ba414038bf49a29c9cad6777f3d2bf73ae5ed15
Kasper Grubbe <kawsper@gmail.com>
2020-02-19 15:43:29 +0000

    Add MiniRacer output to http rails test

--- spec/rails60/app/controllers/application_controller.rb
+++ spec/rails60/app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   def hello
+    miniracer_context = MiniRacer::Context.new
+    miniracer_context.eval "var adder = (a,b)=>a+b;"
+
     render json: {
       message: "Hi!",
       environment: ENV,
+      javascript: miniracer_context.eval("adder(20,22)"),
     }
   end
 end
