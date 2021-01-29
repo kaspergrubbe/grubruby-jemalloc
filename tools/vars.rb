68d998c2c0d306fe01d1c9734d79aad87996fd03
Kasper Grubbe <kawsper@gmail.com>
2021-01-29 10:35:32 +0100

    Upgrade Rubygems to 3.2.7 (from 3.2.3)

--- tools/vars.rb
+++ tools/vars.rb
@@ -4,7 +4,7 @@ require 'open3'
 @grubruby_reponame  = 'grubruby-jemalloc'
 @grubruby_version   = '9006'
 
-@rubygems_version   = '3.2.3'
+@rubygems_version   = '3.2.7'
 @bundler_version    = '2.2.7'
 
 # VERSION, HASH, NEEDS_THPOFF, RAILS_VERSION
