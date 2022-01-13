280d2d909b8bdbb8ec63435c29c55021e346a9bf
Kasper Grubbe <kawsper@gmail.com>
2022-01-13 15:51:36 +0100

    Revert back to miniracer

--- spec/rails61/app/controllers/application_controller.rb
+++ spec/rails61/app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
-require "sq_mini_racer"
+require 'mini_racer'
 
 class ApplicationController < ActionController::Base
   def hello
-    miniracer_context = Sqreen::MiniRacer::Context.new
-    miniracer_context.eval "var adder = (a,b)=>a+b;"
+    miniracer_context = MiniRacer::Context.new
+    miniracer_context.eval 'var adder = (a,b)=>a+b;'
 
     render json: {
-      message: "Hi!",
+      message: 'Hi!',
       environment: ENV,
-      javascript: miniracer_context.eval("adder(20,22)"),
+      javascript: miniracer_context.eval('adder(20,22)')
     }
   end
 end
