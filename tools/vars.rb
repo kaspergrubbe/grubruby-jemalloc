97d5b3df1fb44006c7f582ae16056ba177215bd5
Kasper Grubbe <kawsper@gmail.com>
2022-07-01 21:16:24 +0200

    Update Bundler to 2.3.17 (from 2.3.16)

--- tools/vars.rb
+++ tools/vars.rb
@@ -15,8 +15,8 @@ end
 repo_owner = 'kaspergrubbe'
 repo_name  = 'grubruby-jemalloc'
 version    = '9016'
-bundler    = '2.3.16'
 rubygems   = '3.3.17'
+bundler    = '2.3.17'
 
 @grubruby = Grubruby.new(repo_owner, repo_name, version, rubygems, bundler)
 
