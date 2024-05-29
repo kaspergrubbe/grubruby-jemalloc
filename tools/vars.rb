320f38b5fbd24b2b1ba1c32ae845053fa8cb6679
Kasper Grubbe <kawsper@gmail.com>
2024-05-29 20:52:36 +0200

    Update Bundler to 2.5.11 (from 2.5.9)

--- tools/vars.rb
+++ tools/vars.rb
@@ -15,8 +15,8 @@ end
 repo_owner = 'kaspergrubbe'
 repo_name  = 'grubruby-jemalloc'
 version    = '9026'
-bundler    = '2.5.9'
 rubygems   = '3.5.11'
+bundler    = '2.5.11'
 
 @grubruby = Grubruby.new(repo_owner, repo_name, version, rubygems, bundler)
 
