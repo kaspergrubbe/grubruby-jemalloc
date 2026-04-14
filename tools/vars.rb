e7368238611a140705a8aa6f11e5a6f49e9c4a70
Kasper Grubbe <git@kaspergrubbe.com>
2026-04-14 11:05:26 +0100

    Bump Bundler+Rubygems to 4.0.10

--- tools/vars.rb
+++ tools/vars.rb
@@ -14,10 +14,10 @@ end
 
 repo_owner  = 'kaspergrubbe'
 repo_name   = 'grubruby-jemalloc'
-env_version = ENV["VERSION"] || ENV.fetch("BUILDKITE_TAG") { raise "env VERSION= or BUILDKITE_TAG= must be set" }
-version     = env_version.delete_prefix("v").to_i
-rubygems    = '4.0.9'
-bundler     = '4.0.9'
+env_version = ENV['VERSION'] || ENV.fetch('BUILDKITE_TAG') { raise 'env VERSION= or BUILDKITE_TAG= must be set' }
+version     = env_version.delete_prefix('v').to_i
+rubygems    = '4.0.10'
+bundler     = '4.0.10'
 
 @grubruby = Grubruby.new(repo_owner, repo_name, version, rubygems, bundler)
 
